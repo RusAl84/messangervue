3bf83f1d7fc4343766ad38dd927fc3ad52e255e9
Rusal84 <rusal@bk.ru>
2021-03-23 18:04:55 +0300

    add vuetify

---
+++ src/components/ChatPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+
+    <div id="example-1">
+      <button v-on:click="counter += 1">+1</button>
+      <p>Кнопка выше была нажата {{ counter }} раз</p>
+    </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+  // Модель данных страницы
+  data: () => {
+    return {
+      counter: 0,
+      username: '',
+      message: '',
+      // messages: [
+      //   {"username": "fddffdfdfd", "text": "texttexttexttexttexttext1", "timestamp": "2020-10-15T17:05:12.417897Z"},
+      //   { "UserName":"RusAl","MessageText":"Privet na sto let!!!","TimeStamp":"2021-03-05T18:23:10.932973Z"},
+      messages: [],
+      intervalCtx: null,
+      lastMsgID: 0,
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
