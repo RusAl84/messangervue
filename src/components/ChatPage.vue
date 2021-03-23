34fddf31b48601e3a0db7492f01f15db06c5a9db
Rusal84 <rusal@bk.ru>
2021-03-23 18:29:49 +0300

    Messager

--- src/components/ChatPage.vue
+++ src/components/ChatPage.vue
@@ -1,45 +1,121 @@
 <template>
   <v-container>
+    <v-card>
+      <v-card-title>MyMessenger</v-card-title>
 
-    <div id="example-1">
-      <button v-on:click="counter += 1">+1</button>
-      <p>Кнопка выше была нажата {{ counter }} раз</p>
-    </div>
+      <!-- Панель сообщений -->
+      <v-card-text>
+        <div class="messages-view">
+          <div class="message" v-for="(message, i) in messages" :key="i">
+            <span class="message-date mr-3">{{ new Date(message.TimeStamp).toDateString() }}</span>
+            <span class="message-username mr-5">{{ message.UserName }}</span>
+            <span class="message-text">{{ message.MessageText }}</span>
+          </div>
+        </div>
+      </v-card-text>
 
-    <v-btn
-        class="mx-2"
-        fab
-        dark
-        small
-        color="pink"
-    >
-      <v-icon dark>
-        mdi-heart
-      </v-icon>
-    </v-btn>
-    </v-container>
+      <!-- Панель действий -->
+      <v-card-text>
+        <div>
+          <v-text-field style="width: 40%; min-width: 200px" outlined dense v-model="UserName" label="UserName"/>
+        </div>
+        <div class="d-flex">
+          <v-text-field class="mr-10" outlined dense v-model="MessageText" label="MessageText"/>
+          <v-btn
+              depressed
+              color="primary"
+              @click="onSendClick"
+              :disabled="!MessageText.length"
+          >
+            <v-icon>mdi-send</v-icon>
+          </v-btn>
+        </div>
+      </v-card-text>
+
+
+
+    </v-card>
+
+  </v-container>
 </template>
 
 <script>
+import API from '../api/api';
+
 export default {
   // Модель данных страницы
   data: () => {
     return {
-      counter: 0,
-      username: '',
-      message: '',
+      UserName: '',
+      MessageText: '',
       // messages: [
-      //   {"username": "fddffdfdfd", "text": "texttexttexttexttexttext1", "timestamp": "2020-10-15T17:05:12.417897Z"},
       //   { "UserName":"RusAl","MessageText":"Privet na sto let!!!","TimeStamp":"2021-03-05T18:23:10.932973Z"},
+      //]
       messages: [],
       intervalCtx: null,
       lastMsgID: 0,
     }
+
   },
+
+  // Хук который сработает когда страница создасться
+  mounted() {
+    this.UserName = "Login"
+    this.intervalCtx = setInterval(async () => {
+      try {
+        const msg = await API.getMessage(this.lastMsgID)
+        if (typeof msg === typeof {}) {
+          this.messages.push(msg)
+          this.lastMsgID++
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    }, 1000)
+  },
+
+  methods: {
+    // Реакция на кнопку отправки
+    async onSendClick() {
+      try {
+        await API.sendMessage(this.UserName, this.MessageText)
+        console.log('cleared')
+        this.message = ''
+      } catch (e) {
+        console.error(e)
+      }
+    },
+  },
+  // После уничтожения компонента
+  destroyed() {
+    clearInterval(this.intervalCtx)
+  }
 }
+
 
 </script>
 
-<style scoped>
+<style lang ="sass" scoped>
+.messages-view
+  //border: 1px solid black
+  border-radius: 3px
+  overflow-x: hidden
+  overflow-y: scroll
+  height: 40vh
+
+  .message
+    background: #9fc8ea
+    border-radius: 3px
+    padding: 3px
+    margin: 5px
+
+    .message-date
+      color: rgba(255, 255, 255, 0.9)
+
+    .message-username
+      color: rgba(255, 255, 0, 0.8)
+
+    .message-text
+      color: rgba(0, 0, 0, 0.9)
 
 </style>
